e211e3b9e90ba12a5db380167c53c43692872b57
mxh984511321 <984511321@QQ.com>
2018-02-28 11:37:48 +0800

    commit

---
+++ src/main/java/com/mxh1995/pro/test/类的实例化顺序.java
@@ -0,0 +1,40 @@
+package com.mxh1995.pro.test;
+
+public class 类的实例化顺序 extends Base{
+	
+	public static void main(String[] args) {
+		new Base();
+		new Child();
+	}
+
+}
+
+class Child extends Base{
+	static {
+		System.out.println("孩子静态代码块");
+	}
+	
+	{
+		System.out.println("孩子普通代码块");
+	}
+	
+	Child(){
+		System.out.println("子类构造器");
+	}
+}
+
+class Base {
+
+	Base(){
+		System.out.println("构造器");
+	}
+	
+	static {
+		System.out.println("父亲静态代码块");
+	}
+	
+	{
+		System.out.println("父亲普通代码块");
+	}
+
+}
